f1d8398771728181038bd10636969a517f68f149
Reid Vrana <reidiculous@Reids-MBP.lan>
2020-02-15 15:02:54 -0600

    update app file

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express"),
-  app = express(),
+  app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
-  res.render("landing.ejs");
-});
+// app.get("/", function(req, res) {
+//   res.render("landing.ejs");
+// });
 
 app.listen(process.env.PORT || 5000, process.env.IP);
 
