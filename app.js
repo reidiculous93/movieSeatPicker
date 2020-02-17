40ddcc1a2c330194f8055f2faaa7e79b43a08b62
Reid Vrana <reidiculous@Reids-MBP.lan>
2020-02-17 09:30:06 -0600

    get command

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express"),
   app = express();
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
 app.listen(process.env.PORT || 5000, process.env.IP);
 
 // app.listen(3000, function() {
